35aaae995570668a68196b995e2e780e62a673a4
aliceyunfen <aliceyunfen@gmail.com>
2023-11-17 16:43:44 -0800

    change some files

--- routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const authController = require('./../controllers/authController');
-
-const router = express.Router();
-router.post('/login', authController.login);
-
-module.exports = router;
